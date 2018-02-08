90ec456d0122879b6be9ac68add67282a09e7001
hua <584425439@qq.com>
2018-02-08 16:24:08 +0800

    python3 linux下必须使用pymysql作为驱动的bug修复

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from app.config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
-
+import pymysql
+pymysql.install_as_MySQLdb()
 app = Flask(__name__)
 # 配置 sqlalchemy  数据库驱动://数据库用户名:密码@主机地址:端口/数据库?编码
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
