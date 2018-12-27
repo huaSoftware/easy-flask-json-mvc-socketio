fcbe0f3ff87029ae2503a532b1b5b0c2579ddb2e
华霆 <584425439@qq.com>
2018-12-27 14:07:41 +0800

    增加koro1FileHeader格式化注释

--- app/Vendor/Log.py
+++ app/Vendor/Log.py
@@ -1,9 +1,15 @@
+'''
+@Author: hua
+@Date: 2018-11-22 16:53:44
+@LastEditors: hua
+@LastEditTime: 2018-12-27 14:04:39
+'''
 
 import logging
 import time
 
-def log():
 
+def log():
     log = logging.getLogger("error_msg")
     log.setLevel(logging.DEBUG)
     # 建立一个filehandler来把日志记录在文件里，级别为debug以上
